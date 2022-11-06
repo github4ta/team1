ca4b9ed14e135ee20fb7332a6f03adc79a2a8562
Viktor Marchuk <viktor.m77.77@mail.ru>
2022-11-06 19:51:00 +0300

    add method testCalculateThePercentageOfNumber

--- src/test/java/by/itcademy/team1/CalculatorTest.java
+++ src/test/java/by/itcademy/team1/CalculatorTest.java
@@ -1,4 +1,20 @@
 package by.itcademy.team1;
 
-public class CalculatorTest {
+import by.itacademy.team1.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest{
+    @Test
+    public void testCalculateThePercentageOfNumber(){
+        Calculator calculator = new Calculator();
+        double number = 100;
+        double percent = 10;
+        double expected = 10;
+
+        double actual = calculator.calculateThePercentageOfNumber(percent, number);
+
+        Assert.assertEquals(expected, actual, 0.1);
+
+    }
 }
